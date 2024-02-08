d8ef6fe074c8a31ebc16305cc4a72df9548a6f8b
Muhammad Awais <muhammad.awais@systempavers.com>
2024-02-09 01:51:52 +0500

    form done

--- vue-frontend/src/components/DataCollectionForm.vue
+++ vue-frontend/src/components/DataCollectionForm.vue
@@ -1,11 +1,41 @@
 <template>
   <v-container class="fill-height">
     <v-responsive class="align-center text-center fill-height">
-      <div class="text-body-2 font-weight-light mb-n1">Data Collection Form</div>
+      <v-form @submit.prevent="submitForm">
+        <v-row>
+          <v-col xl="12">
+            <h1>Data Collection Form</h1>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col xl="12">
+            <v-text-field v-model="formData.firstName" label="Vorname" required hide-details class="mb-6"></v-text-field>
+            <v-text-field v-model="formData.lastName" label="Nachname" required hide-details class="mb-6"></v-text-field>
+            <v-text-field v-model="formData.street" label="Strasse" required hide-details class="mb-6"></v-text-field>
+            <v-text-field v-model="formData.housenr" label="Hausnr." required hide-details class="mb-6"></v-text-field>
+            <v-text-field v-model="formData.zipcode" label="PLZ" required hide-details class="mb-6"></v-text-field>
+            <v-text-field v-model="formData.city" label="Stadt" required hide-details class="mb-6"></v-text-field>
+          </v-col>
+        </v-row>
+        <v-btn type="submit">Submit</v-btn>
+      </v-form>
     </v-responsive>
   </v-container>
 </template>
 
 <script setup>
-  //
+import { ref } from 'vue';
+
+const formData = {
+  firstName: ref(''),
+  lastName: ref(''),
+  street: ref(''),
+  housenr: ref(''),
+  zipcode: ref(''),
+  city: ref('')
+};
+
+const submitForm = () => {
+  // Submit form logic here
+};
 </script>
